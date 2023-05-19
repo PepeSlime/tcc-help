e0de84930bd410e31509050302524559941fc2b8
Pedro Henrique <pepegemeo8@gmail.com>
2023-05-19 20:26:59 -0300

    .

--- home.js
+++ home.js
@@ -1,4 +1,11 @@
 function mostarMenu() {
-    
+    let menu = document.querySelector("#menu");
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden")
+    } else {
+
+
+        menu.classList.remove("hidden");
+    }
 }
 
